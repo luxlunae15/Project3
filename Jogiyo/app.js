4128540985d4300b801a29ba24751daed13b6e1c
YeChanSong <54590290+YeChanSong@users.noreply.github.com>
2020-06-15 21:38:26 +0900

    app.js 수정

--- Jogiyo/app.js
+++ Jogiyo/app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var join = require('./routes/joinForm');
+var login = require('./routes/login');
+
+var seller = require('./routes/seller');
 
 var app = express();
 
@@ -21,6 +25,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/join', join);
+app.use('/login', login);
+app.use('/seller', seller);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
